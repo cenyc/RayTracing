71464f23f19fcab5e752fbd3e145a9cca6741fa9
cenyc <yccen@m.scnu.edu.cn>
2020-07-17 11:03:04 +0800

    给球体添加漫反射光照模型

--- main.cpp
+++ main.cpp
@@ -11,15 +11,22 @@
 #include "sphere.h"
 #include "hitable_list.h"
 #include "camera.h"
-#include "float.h"
 
 using namespace std;
 
+vec3 random_in_unit_shpere() {
+    vec3 p;
+    do {
+        p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
+    }while (p.squared_length() >= 1.0);
+    return p;
+}
 
 vec3 color(const ray& r, hitable *world) {
     hit_record rec;
-    if (world->hit(r, 0.0, MAXFLOAT, rec)) {
-        return 0.5 * vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+    if (world->hit(r, 0.001, MAXFLOAT, rec)) {
+        vec3 target = rec.p + rec.normal + random_in_unit_shpere();
+        return 0.5 * color(ray(rec.p, target-rec.p), world);
     }else {
         vec3 unit_direction = unit_vector(r.direction());
         float t = 0.5 * (unit_direction.y() + 1.0);
@@ -27,10 +34,11 @@ vec3 color(const ray& r, hitable *world) {
     }
 }
 
+
 int main(int argc, const char * argv[]) {
     int nx = 800;
     int ny = 800;
-    int ns = 100; // 在每个像素点附近随机采样次数（减少锯齿）
+    int ns = 1; // 在每个像素点附近随机采样次数（减少锯齿）
 
     ofstream out("out.ppm");
     out << "P3\n" << nx << " " << ny << "\n255\n";
@@ -52,6 +60,7 @@ int main(int argc, const char * argv[]) {
             }
 
             col /= float (ns);
+            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
